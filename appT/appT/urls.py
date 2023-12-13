72c6c04c646a094650c56a50299f5616af10bc8e
Alessandro Sefani <alessandro.stefani@omnicon.it>
2023-12-13 10:03:20 +0100

    lanciato il comando migrate

--- appT/appT/urls.py
+++ appT/appT/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('data_display/', include('data_display.urls')),
     path('admin/', admin.site.urls),
 ]
--- appT/data_display/models.py
+++ appT/data_display/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class TimeSeries(models.Model):
+    dateTime = models.DateTimeField()
+    value = models.FloatField()
+
+    def value_was_too_high(self):
+        return self.value >= 300
+    
+    def value_was_too_low(self):
+        return self.value <= 130
+    
+    def __str__(self) -> str:
+        return f"{{{self.dateTime}}}:{{{self.value}}}"
